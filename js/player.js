43165362e07f49017489174961d48d4fb993f537
RHN007 <rakibul.nayon@outlook.com>
2022-08-23 19:00:03 +0600

    plling

--- js/player.js
+++ js/player.js
@@ -89,7 +89,6 @@ var sunilNarine = {
    
    function displayAllArticles(arr) {
      
-     
       for (let i = 0; i < arr.length; i++) {
           const element = arr[i];
           displayPlayers(element);
